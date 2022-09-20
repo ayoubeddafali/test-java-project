fc8d8144720c549682be3f8700591460e8ae1af1
ayoubensalem <ayoubensalem@gmail.com>
2022-09-20 11:29:16 +0100

    test

--- .github/actions/badge/index.js
+++ .github/actions/badge/index.js
@@ -1,5 +1,55 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const dataForge = require('data-forge');
+require('data-forge-fs');
+
+
+function getReportData() {
+  const result = {};
+  let data = dataForge.readFileSync('/tmp/arvos-report.csv').parseCSV().renameSeries(
+    { "ID": "id",
+      "Vulnerability": "vulnerability",
+      "Vulnerability Detail": "detail",
+      "Score": "score",
+      "Description": "description",
+      "Invoked Class": "class",
+      "Invoked Method": "method",
+      "Package name": "package",
+      "Github Repository": "repo",
+      "Package manager": "manager",
+      "Version range": "range",
+      "Stacktrace": "stacktrace"
+    })
+
+  result.vulns = data.toArray();
+  result.vulns_count = data.getSeries('vulnerability').distinct().count();
+  result.symbols_count = data.count()
+  result.vuln_packages = data.getSeries('package').distinct().count()
+
+  const sumObjectsByKey = (...objs) => {
+    const res = objs.reduce((a, b) => {
+        for (let k in b) {
+          if (b.hasOwnProperty(k))
+          a[k] = (a[k] || 0) + b[k];
+        }
+        return a;
+    }, {});
+    return res;
+  }
+
+  let scoreCountInit = {'CRITICAL':0, 'HIGH': 0, 'MEDIUM': 0, 'LOW':0}
+  let uniqVulns = data.distinct(vuln => vuln.vulnerability)
+  let arr = uniqVulns.getSeries('score').toArray()
+  var scoreCount = arr.reduce((acc, val) => {
+    acc[val] = acc[val] === undefined ? 1 : acc[val] += 1;
+    return acc;
+  }, {});
+  
+  let scores = sumObjectsByKey(scoreCountInit, scoreCount )
+  result.pieData = [scores['CRITICAL'], scores['HIGH'], scores['MEDIUM'], scores['LOW']]
+
+  return result;
+}
 
 async function donwloadArvosReport(octokit, context) {
 
@@ -44,9 +94,11 @@ async function run() {
   const octokit = github.getOctokit(myToken)
   const context = github.context;
 
-  donwloadArvosReport(octokit, context).then((res) => {
+  donwloadArvosReport(octokit, context).then(() => {
     console.log("Hello")
-    console.log(res)
+    const data = getReportData()
+    console.log(data)
+    
   })
 
 }
